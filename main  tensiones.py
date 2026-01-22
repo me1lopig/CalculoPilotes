b6e986ce957a73acbefac99084501b5610d2116b
GermanLopez <me1lopig@uco.es>
2026-01-22 12:36:46 +0100

    Separacion del codigo de generacion de tensiones verticales

---
+++ main  tensiones.py
@@ -0,0 +1,41 @@
+# cálculo de tensiones verticales en el terreno
+
+
+
+# version 22/12/2024
+# Germán López Pineda
+# Ingeniero de Caminos Canales y Puertos UGR
+# Master en Matemática Computacional UJI
+# Master en Ingeniería del Terreno UCO
+# Grupo de investigación RNM 244 Ingeniería Ambiental y Geofísica Universidad de Córdoba
+# me1lopig@uco.es
+# rocasysuelos@gmail.com
+
+
+
+# llamada a las librerias
+import numpy as np # librería para cálculos matematicos
+import os
+
+# llamada a librerias definidas
+import funcionesCalculo as ft # libreria de funciones auxiliares y de cálculo
+
+
+
+# ruta absoluta de los archivos de calculo
+carpeta=os.getcwd()
+carpeta=os.path.join(carpeta, 'Data/')
+archivo_terreno=os.path.join(carpeta,'datos_terreno_5.xlsx')
+archivo_pilotes=os.path.join(carpeta,'datos_pilotes.xlsx')
+
+# importacion de datos del terreno
+espesor,cotas,az,nivel_freatico,pe_seco,pe_saturado,cu,cohesion,fi,tipo_datos,tipo_calculo=ft.datos_terreno(archivo_terreno)
+
+
+# creacion del directorio de trabajo
+directorio=ft.crea_directorio()
+
+# graficas de las tensiones totales, efectivas y de poro del terreno según al archivo datos_terreno.xlsx
+ft.grafica_tensiones(cotas,pe_seco,pe_saturado,nivel_freatico,directorio)
+
+
